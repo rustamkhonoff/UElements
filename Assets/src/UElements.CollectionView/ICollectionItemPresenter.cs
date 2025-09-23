3e6f91b8f2a4ae02edd61904cf4b244c1a081f2e
Shokhrukhkhon Rustamkhonov <rustamkhonoff@sabgames.org>
2025-09-23 16:12:41 +0500

    collection updates

--- Assets/src/UElements.CollectionView/ICollectionItemPresenter.cs
+++ Assets/src/UElements.CollectionView/ICollectionItemPresenter.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace UElements.CollectionView
 {
     public interface ICollectionModelPresenter<out TModel, out TView>
@@ -8,6 +10,30 @@ namespace UElements.CollectionView
         void Initialize();
         void Dispose();
     }
+
+    public class CollectionModelPresenter<TModel, TView> : CollectionModelPresenterBase<TModel, TView> where TView : ModelElement<TModel>
+    {
+        private readonly Action<TModel, TView> m_init;
+        private readonly Action<TModel, TView> m_dispose;
+
+        public CollectionModelPresenter(TModel model, TView view, Action<TModel, TView> init = null, Action<TModel, TView> dispose = null) :
+            base(model, view)
+        {
+            m_init = init;
+            m_dispose = dispose;
+        }
+
+        public override void Initialize()
+        {
+            m_init?.Invoke(Model, View);
+        }
+
+        public override void Dispose()
+        {
+            m_dispose?.Invoke(Model, View);
+        }
+    }
+
     public abstract class CollectionModelPresenterBase<TModel, TView> : ICollectionModelPresenter<TModel, TView>
         where TView : ModelElement<TModel>
     {
