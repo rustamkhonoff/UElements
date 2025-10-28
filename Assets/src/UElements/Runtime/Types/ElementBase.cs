e196e4932954a0ca370c514e93853973d07d0919
Shokhrukhkhon Rustamkhonov <rustamkhonoff@sabgames.org>
2025-10-28 14:44:01 +0500

    0.0.6 upd

--- Assets/src/UElements/Runtime/Types/ElementBase.cs
+++ Assets/src/UElements/Runtime/Types/ElementBase.cs
@@ -1,17 +1,20 @@
-using System.Threading;
-using UnityEngine;
 using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace UElements
 {
     public abstract class ElementBase : MonoBehaviour, IDisposable
     {
         public event Action Disposing;
+
         public IElementController ElementController { get; set; } = new DefaultElementController();
+
         private CancellationTokenSource m_cancellationTokenSource = new();
-        private bool m_disposed;
         private bool m_closed;
+        private bool m_disposed;
+
         protected IElements Elements { get; private set; }
 
         internal UniTask Initialize(IElements elements)
@@ -21,51 +24,50 @@ namespace UElements
             return InitializeAsync();
         }
 
-        public UniTask Show(Action callback)
-        {
-            return Show(callback, ElementController);
-        }
+        #region Public API
 
-        public async UniTask Show(Action callback, IElementController customController)
+        public async UniTask Show(Action callback = null, IElementController customController = null)
         {
-            await customController.Show(this);
+            var controller = customController ?? ElementController;
+            await controller.Show(this);
             callback?.Invoke();
         }
 
-        public UniTask Hide(Action callback)
+        public async UniTask Hide(Action callback = null, IElementController customController = null)
         {
-            return Hide(callback, ElementController);
-        }
-
-        public async UniTask Hide(Action callback, IElementController customController)
-        {
-            await customController.Hide(this);
+            var controller = customController ?? ElementController;
+            await controller.Hide(this);
             callback?.Invoke();
         }
 
-        public UniTask Close(Action callback)
-        {
-            return Close(callback, ElementController);
-        }
-
-        public async UniTask Close(Action callback, IElementController customController)
+        public async UniTask Close(Action callback = null, IElementController customController = null)
         {
             if (m_closed) return;
             m_closed = true;
 
-            await customController.Hide(this);
+            IElementController controller = customController ?? ElementController;
+
+            try
+            {
+                await controller.Hide(this);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning($"[ElementBase] Hide failed: {e.Message}");
+            }
+
             callback?.Invoke();
 
             CleanupBase();
             Destroy(gameObject);
         }
 
-        public virtual void Initialize() { }
+        #endregion
 
-        public virtual UniTask InitializeAsync()
-        {
-            return UniTask.CompletedTask;
-        }
+        #region Lifecycle
+
+        public virtual void Initialize() { }
+        public virtual UniTask InitializeAsync() => UniTask.CompletedTask;
 
         protected virtual void OnDisposing() { }
 
@@ -74,22 +76,20 @@ namespace UElements
             if (m_disposed) return;
             m_disposed = true;
 
+            Close().Forget();
             OnDisposing();
-
-            if (!m_closed)
-            {
-                CleanupBase();
-                Destroy(gameObject);
-            }
         }
 
         private void CleanupBase()
         {
-            m_cancellationTokenSource?.Cancel();
-            m_cancellationTokenSource?.Dispose();
-            m_cancellationTokenSource = null;
-            Elements = null;
+            if (m_cancellationTokenSource != null)
+            {
+                m_cancellationTokenSource.Cancel();
+                m_cancellationTokenSource.Dispose();
+                m_cancellationTokenSource = null;
+            }
 
+            Elements = null;
             Disposing?.Invoke();
         }
 
@@ -97,8 +97,9 @@ namespace UElements
 
         private void OnDestroy()
         {
-            if (!m_disposed)
-                Dispose();
+            if (!m_disposed) Dispose();
         }
+
+        #endregion
     }
 }
